32a5555adabc3bd379224754be2beaf37db79fdc
tianshengjie <tianshengjie@howzhi.com>
2018-01-05 00:03:48 +0800

    add test command

---
+++ src/Command/TestCommand.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand extends Command
+{
+    protected function configure()
+    {
+       $this
+        // the name of the command (the part after "bin/console")
+        ->setName('app:test-command')
+
+        // the short description shown while running "php bin/console list"
+        ->setDescription('dump test')
+
+        // the full command description shown when running the command with
+        // the "--help" option
+        ->setHelp('--help--')
+    ;  
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('test!');
+    }
+}
